fcd3d576726fd78df920473216444c640f02cb56
xuwensi <xuwensi0515@gmail.com>
2021-05-30 16:38:41 +0800

    what not

---
+++ app/src/main/java/edu/uw/wensix/leetcodelegend/model/Problem.kt
@@ -0,0 +1,16 @@
+package edu.uw.wensix.leetcodelegend.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Problem(
+    val id: Int,
+    val number: Int,
+    val title: String,
+    val date: String,
+    val difficulty: String,
+    val note: String,
+    val durationSecond: Int,
+    val notifyDate: String,
+):Parcelable
